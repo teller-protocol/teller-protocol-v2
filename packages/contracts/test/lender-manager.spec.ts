34c80b9787fb3071d45eaf80e099c964b9301a25
Syed <sy.peeran@gmail.com>
2022-11-11 19:52:09 +0530

    additional tests

--- packages/contracts/test/lender-manager.spec.ts
+++ packages/contracts/test/lender-manager.spec.ts
@@ -49,6 +49,8 @@ describe('LenderManager', () => {
   let lenderManager: LenderManager
   let lender: Signer
   let borrower: Signer
+  let newLender: Signer
+  let newLenderAddress: string
 
   before(async () => {
     const result = await setup()
@@ -56,6 +58,8 @@ describe('LenderManager', () => {
 
     lender = await getNamedSigner('lender')
     borrower = await getNamedSigner('borrower')
+    newLender = await getNamedSigner('lender2')
+    newLenderAddress = await newLender.getAddress()
 
     const marketRegistry = result.marketRegistry
 
@@ -75,7 +79,7 @@ describe('LenderManager', () => {
         paymentDefaultDuration,
         loanExpirationTime,
         0,
-        false,
+        true,
         false,
         '0',
         'uri://'
@@ -84,15 +88,41 @@ describe('LenderManager', () => {
 
   describe('getActiveLoanLender', () => {
     it('should return the active lender for a loan from the Lender Manager', async () => {
-      const { tellerV2, bidId } = await acceptedBid({ borrower, lender })
-
-      const bidData = await tellerV2.bids(bidId)
+      const { bidId } = await acceptedBid({ borrower, lender })
 
       const activeLender = await lenderManager.callStatic.getActiveLoanLender(
         bidId
       )
 
       expect(await lender.getAddress()).to.eql(activeLender)
+    })
+  })
+
+  describe('setNewLender', () => {
+    it('should be able to set a new active loan lender', async () => {
+      const { bidId, tellerV2 } = await acceptedBid({ borrower, lender })
+      const bidData = await tellerV2.bids(bidId)
+
+      await lenderManager
+        .connect(lender)
+        .setNewLender(bidId, newLenderAddress, bidData.marketplaceId)
+        .should.emit(lenderManager, 'NewLenderSet')
+        .withArgs(newLenderAddress, bidId)
+
+      const activeLender = await lenderManager.callStatic.getActiveLoanLender(
+        bidId
+      )
+
+      expect(activeLender).to.eql(newLenderAddress)
+    })
+    it('should not be able to set new active loan lender as not the loan owner', async () => {
+      const { bidId, tellerV2 } = await acceptedBid({ borrower, lender })
+      const bidData = await tellerV2.bids(bidId)
+
+      await lenderManager
+        .connect(borrower)
+        .setNewLender(bidId, newLenderAddress, bidData.marketplaceId)
+        .should.be.revertedWith('Not loan owner')
     })
   })
 })

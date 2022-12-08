cf1d567c9f2b4f6b0feac779bad0d61b0e99b7e1
Syed <sy.peeran@gmail.com>
2022-12-07 19:21:39 -0500

    escrow creation integration test

--- packages/contracts/contracts/tests/TellerV2_Test.sol
+++ packages/contracts/contracts/tests/TellerV2_Test.sol
@@ -17,38 +17,146 @@ import "../EAS/TellerAS.sol";
 import "../mock/WethMock.sol";
 import "../interfaces/IWETH.sol";
 
-contract TellerV2_Test is Testable, TellerV2 {
+import { User } from "./Test_Helpers.sol";
+
+import "../escrow/CollateralEscrowV1.sol";
+import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import "../LenderCommitmentForwarder.sol";
+import "./resolvers/TestERC20Token.sol";
+import "../escrow/CollateralEscrowFactory.sol";
+
+import "@mangrovedao/hardhat-test-solidity/test.sol";
+
+contract TellerV2_Test is Testable {
     User private marketOwner;
     User private borrower;
     User private lender;
 
-    WethMock wethMock;
+    TellerV2 tellerV2;
 
-    constructor() TellerV2(address(address(0))) {}
+    WethMock wethMock;
+    TestERC20Token daiMock;
+
+    uint256 marketId1;
 
     function setup_beforeAll() public {
+        // Deploy test tokens
         wethMock = new WethMock();
+        daiMock = new TestERC20Token('Dai', 'DAI', 10000000);
 
-        marketOwner = new User(this, wethMock);
-        borrower = new User(this, wethMock);
-        lender = new User(this, wethMock);
+        // Deploy MarketRegistry & ReputationManager
+        IMarketRegistry marketRegistry = IMarketRegistry(new MarketRegistry());
+        IReputationManager reputationManager = IReputationManager(new ReputationManager());
+        // Deploy Escrow factory
+        CollateralEscrowV1 escrowImplementation = new CollateralEscrowV1();
+        UpgradeableBeacon escrowBeacon = new UpgradeableBeacon(address(escrowImplementation));
 
-        lenderCommitmentForwarder = address(0);
-        marketRegistry = IMarketRegistry(new MarketRegistry());
-        reputationManager = IReputationManager(new ReputationManager());
+        tellerV2 = new TellerV2(address(0));
+        // Deploy LenderCommitmentForwarder
+        LenderCommitmentForwarder lenderCommitmentForwarder = new LenderCommitmentForwarder(
+            address(tellerV2),
+            address(marketRegistry)
+        );
+
+        address[] memory lendingTokens = new address[](2);
+        lendingTokens[0] = address(wethMock);
+        lendingTokens[1] = address(daiMock);
+
+        // Initialize protocol
+        tellerV2.initialize(
+            50,
+            address(marketRegistry),
+            address(reputationManager),
+            address(lenderCommitmentForwarder),
+            lendingTokens,
+            address(escrowBeacon)
+        );
+
+        // Instantiate users & balances
+        marketOwner = new User(tellerV2, wethMock);
+        borrower = new User(tellerV2, wethMock);
+        lender = new User(tellerV2, wethMock);
+
+        uint256 balance = 50000;
+        payable(address(borrower)).transfer(balance);
+        payable(address(lender)).transfer(balance*10);
+        borrower.depositToWeth(balance);
+        lender.depositToWeth(balance*10);
+
+        daiMock.transfer(address(lender), balance*10);
+        // Approve lender's dai
+        lender.addAllowance(
+            address(daiMock),
+            address(tellerV2),
+            balance*10
+        );
+
+        // Create a market
+        marketId1 = marketOwner.createMarket(
+            address(marketRegistry),
+            8000,
+            7000,
+            5000,
+            500,
+            false,
+            false,
+            V2Calculations.PaymentType.EMI,
+            "uri://"
+        );
     }
-}
 
-contract User {
-    TellerV2 public immutable tellerV2;
-    WethMock public immutable wethMock;
+    function submitCollateralBid() public returns(uint256 bidId_) {
+        ICollateralEscrowV1.Collateral memory collateralInfo;
+        collateralInfo._amount = 10;
+        collateralInfo._tokenId = 0;
+        collateralInfo._collateralType = ICollateralEscrowV1.CollateralType.ERC20;
+        collateralInfo._collateralAddress = address(wethMock);
 
-    constructor(TellerV2 _tellerV2, WethMock _wethMock) {
-        tellerV2 = _tellerV2;
-        wethMock = _wethMock;
+        uint256 bal = wethMock.balanceOf(address(borrower));
+
+        // Increase allowance
+        // Approve borrower's weth
+        borrower.addAllowance(
+            address(wethMock),
+            address(tellerV2),
+            collateralInfo._amount
+        );
+
+        bidId_ = borrower.submitCollateralBid(
+            address(daiMock),
+            marketId1,
+            100,
+            10000,
+            500,
+            "metadataUri://",
+            address(borrower),
+            address(wethMock),
+            collateralInfo
+        );
     }
 
-    function depositToWeth(uint256 amount) public {
-        wethMock.deposit{ value: amount }();
+    function acceptBid(uint256 _bidId) public {
+        // Accept bid
+        lender.acceptBid(
+            _bidId
+        );
+    }
+
+    function collateralEscrow_test() public {
+        uint256 bidId = submitCollateralBid();
+        acceptBid(bidId);
+
+        // Get newly created escrow
+        address escrowAddress = tellerV2._escrows(bidId);
+        ICollateralEscrowV1 escrow = ICollateralEscrowV1(escrowAddress);
+
+        uint256 storedBidId = escrow.getBid();
+
+        // Test that the created escrow has the same bidId and collateral stored
+        Test.eq(
+            bidId,
+            storedBidId,
+            'Collateral escrow was not created'
+        );
     }
 }

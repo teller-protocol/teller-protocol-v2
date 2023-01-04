94d1717538bca3087b9c716c697bb8aa562a723e
Admazzola <admazzola@gmail.com>
2023-01-04 11:38:19 -0500

    more config

--- packages/contracts/test/contract-upgrade.spec.ts
+++ packages/contracts/test/contract-upgrade.spec.ts
@@ -42,7 +42,7 @@ const contractConfig: any = {
     proxyAdminAddress: '0xcCfFa4e4cBE27D92f926E7B2e396a772Ddf0F2B2',
     tellerV2Address: '0x00182FdB0B880eE24D428e3Cc39383717677C37e',
     marketRegistry: '0x5e30357d5136bc4bfadba1ab341d0da09fe7a9f1',
-    wethAaddress: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    wethAddress: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
   },
  
   goerli: {   
@@ -50,7 +50,7 @@ const contractConfig: any = {
     proxyAdminAddress: '0xD27FD721635de713dA155260F4Ca69b717B76Ef9',
     tellerV2Address: '0x1D0eBC060f2897dAc14E482De600cea7896c0A3E',
     marketRegistry: '0x1815f01083afd110373e952b6488f33b048f096b',
-    wethAaddress: '0xffc94fb06b924e6dba5f0325bbed941807a018cd',
+    wethAddress: '0xffc94fb06b924e6dba5f0325bbed941807a018cd',
   },
 }
 
@@ -78,13 +78,15 @@ describe('Contract Upgrade', () => {
         signer
       )
 
-      const upgradeVersion = await tellerV2Contract.CURRENT_CODE_VERSION()
+      await upgradeTellerV2Proxy(hre)
+
+     /* const upgradeVersion = await tellerV2Contract.CURRENT_CODE_VERSION()
 
       if (upgradeVersion != LATEST_CODE_VERSION) {
 
         //upgrade the contract ! 
         await upgradeTellerV2Proxy(hre)
-      }
+      }*/
     }
   })
 
--- packages/contracts/test/helpers/upgrade-utils.ts
+++ packages/contracts/test/helpers/upgrade-utils.ts
@@ -1,8 +1,10 @@
 import { getTokens } from 'config'
 import { BigNumber, Contract, ContractFactory, Signer, Wallet } from 'ethers'
-import hre, { ethers, getNamedSigner, network } from 'hardhat'
+import hre, { ethers, upgrades, getNamedSigner, network } from 'hardhat'
 import { DeployFunction } from 'hardhat-deploy/dist/types'
+ 
 import { deploy } from 'helpers/deploy-helpers'
+import { isInitialized } from 'helpers/oz-contract-helpers'
 import {
   upgradeProxyAdminWithImplementation,
   upgradeProxyWithImplementation,
@@ -15,16 +17,20 @@ const contractConfig: any = {
     deployerAddress: '0xafe87013dc96ede1e116a288d80fcaa0effe5fe5',
     proxyAdminAddress: '0xcCfFa4e4cBE27D92f926E7B2e396a772Ddf0F2B2',
     tellerV2Address: '0x00182FdB0B880eE24D428e3Cc39383717677C37e',
-    marketRegistry: '0x5e30357d5136bc4bfadba1ab341d0da09fe7a9f1',
-    wethAaddress: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    marketRegistryAddress: '0x5e30357d5136bc4bfadba1ab341d0da09fe7a9f1',
+    wethAddress: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    reputationManagerAddress: '0xF0da18a5b53a6C0c4e763013A8DAEeD895A93627',
+    lenderCommitmentForwarderAddress: '0x17A8e82351661DFD568FEE6D7c38695b67e1e924'
   },
  
   goerli: {   
     deployerAddress: '0x04cd8462cffe5eddfb923fce634e13bfbdc83886',
     proxyAdminAddress: '0xD27FD721635de713dA155260F4Ca69b717B76Ef9',
     tellerV2Address: '0x1D0eBC060f2897dAc14E482De600cea7896c0A3E',
-    marketRegistry: '0x1815f01083afd110373e952b6488f33b048f096b',
-    wethAaddress: '0xffc94fb06b924e6dba5f0325bbed941807a018cd',
+    marketRegistryAddress: '0x1815f01083afd110373e952b6488f33b048f096b',
+    wethAddress: '0xffc94fb06b924e6dba5f0325bbed941807a018cd',
+    reputationManagerAddress: '0xF4d6441D421d1dcFB2d6B3E4490C9161b4529920',
+    lenderCommitmentForwarderAddress: '0xC2418B857F9147AF5eB4517B23e9a966b368328f'
   },
 }
 
@@ -34,7 +40,15 @@ export const upgradeTellerV2Proxy: DeployFunction = async (hre) => {
   await hre.evm.impersonate(config.deployerAddress)
   const signer = ethers.provider.getSigner(config.deployerAddress)
 
-  /*
+  
+  const protocolFee = 5 // 0.05%
+
+  const tokens = await getTokens(hre)
+  const lendingTokens = [tokens.all.DAI, tokens.all.USDC, tokens.all.WETH]
+  if ('USDCT' in tokens.all) {
+    lendingTokens.push(tokens.all.USDCT)
+  }
+
   const trustedForwarder = await deploy({
     contract: 'MetaForwarder',
     skipIfAlreadyDeployed: false,
@@ -49,9 +63,35 @@ export const upgradeTellerV2Proxy: DeployFunction = async (hre) => {
     },
     hre,
   })
-  */
+    
+ 
+  
+    const collateralEscrowV1 = await hre.ethers.getContractFactory(
+      'CollateralEscrowV1'
+    )
+    // Deploy escrow beacon implementation
+    const collateralEscrowBeacon = await upgrades.deployBeacon(collateralEscrowV1)
+    await collateralEscrowBeacon.deployed()
+  
+    const collateralManager = await deploy({
+      contract: 'CollateralManager',
+      args: [],
+      proxy: {
+        proxyContract: 'OpenZeppelinTransparentProxy',
+      },
+      hre,
+    })
+    const collateralManagerIsInitialized = await isInitialized(
+      collateralManager.address
+    )
+    if (!collateralManagerIsInitialized) {
+      await collateralManager.initialize(
+        collateralEscrowBeacon.address,
+        config.tellerV2Address
+      )
+    }
 
-   
+
 
   //deploy the implementation
 
@@ -60,13 +100,8 @@ export const upgradeTellerV2Proxy: DeployFunction = async (hre) => {
     existingProxyAddress: config.tellerV2Address,
     proxyAdminAddress: config.proxyAdminAddress,
     signer,
-    args: [
-      config.tellerV2Address,
-      config.marketRegistry,
-      config.marketId,
-      config.wethAaddress,
-      trustedForwarder.address,
-      config.craSignerPublicAddress,
+    args: [    
+      trustedForwarder.address, 
     ],
     proxy: {
       proxyContract: 'OpenZeppelinTransparentProxy',
@@ -74,9 +109,12 @@ export const upgradeTellerV2Proxy: DeployFunction = async (hre) => {
         init: {
           methodName: 'initialize',
           args: [
-            config.cryptopunksMarketAddress,
-            seaportEscrowBuyer.address,
-            punksEscrowBuyer.address,
+            protocolFee,
+            config.marketRegistryAddress,
+            config.reputationManagerAddress, 
+            config.lenderCommitmentForwarderAddress, 
+            lendingTokens,
+            collateralManager.address   
           ],
         },
         onUpgrade: {

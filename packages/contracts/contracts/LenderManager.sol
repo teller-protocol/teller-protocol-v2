b8b45563f431880beb93d9f1a3e7acc047009da6
andy <andrew@teller.org>
2023-02-09 16:30:00 -0500

    cleaning up

--- packages/contracts/contracts/LenderManager.sol
+++ packages/contracts/contracts/LenderManager.sol
@@ -21,14 +21,7 @@ ERC721Upgradeable,
 ILenderManager
 {
     IMarketRegistry public immutable marketRegistry;
-
-    // Mapping of loans to current active lenders - using erc721 now 
-   // mapping(uint256 => address) internal _loanActiveLender;
  
-
-    /** Events **/
-    event NewLoanRegistered(address indexed newLender, uint256 bidId);
-
     constructor( address _marketRegistry)
     //ERC2771ContextUpgradeable(_trustedForwarder)
     {
@@ -56,45 +49,12 @@ ILenderManager
     override
     onlyOwner
     { 
-        _mint(_newLender,_bidId);
-
-        emit NewLoanRegistered(_newLender, _bidId);
+        _mint(_newLender,_bidId); 
     }
 
     
 
-    /**
-     * @notice Returns the address of the lender that owns a given loan/bid.
-     * @param _bidId The id of the bid to return the lender for
-     * @return The address of the lender.
-     */
-    function getActiveLoanLender(uint256 _bidId)
-    external
-    view
-    override
-    returns (address)
-    {
-        return _getActiveLender(_bidId);
-    }
 
-    /**
-     * @notice Returns the address of the lender that owns a given loan/bid.
-     * @param _bidId The id of the bid to return the lender for
-     * @return lender_ The address of the lender.
-     */
-    function _getActiveLender(uint256 _bidId)
-    internal
-    view
-    returns (address lender_)
-    {
-        if(_exists(_bidId)){
-            return ownerOf(_bidId);
-        }else{
-            //CAUTION: If a loan NFT is burned, this branch of code will be executed
-            return ITellerV2(owner()).getLoanLender(_bidId);
-        }
-       
-    }
 
     /**
      * @notice Returns the address of the lender that owns a given loan/bid.

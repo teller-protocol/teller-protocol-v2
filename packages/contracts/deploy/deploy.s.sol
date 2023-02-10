aef0b2d288c0cfc482b86f6aecdfe2dd1fa8f1c5
andy <andrew@teller.org>
2023-02-10 11:51:51 -0500

    examples

--- packages/contracts/deploy/deploy.s.sol
+++ packages/contracts/deploy/deploy.s.sol
@@ -1,5 +1,12 @@
 pragma solidity >=0.8.0 <0.9.0;
 
+/*
+See examples 
+https://github.com/maple-labs/maple-core-v2/tree/main/scripts
+
+
+*/
+
 import { Script } from "forge-std/Script.sol";
 import { Foo } from "../src/Foo.sol";
 

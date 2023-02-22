2d1fd37529192d95567f312fec35930832a8f4e4
Noah Passalacqua <noah@teller.org>
2023-02-22 15:08:52 -0600

    move commitment borrowers event

--- packages/contracts/contracts/LenderCommitmentForwarder.sol
+++ packages/contracts/contracts/LenderCommitmentForwarder.sol
@@ -243,8 +243,6 @@ contract LenderCommitmentForwarder is TellerV2MarketForwarder {
     ) public commitmentLender(_commitmentId) {
         delete commitmentBorrowersList[_commitmentId];
         _addBorrowersToCommitmentAllowlist(_commitmentId, _borrowerAddressList);
-
-        emit UpdatedCommitmentBorrowers(_commitmentId);
     }
 
     /**
@@ -258,6 +256,9 @@ contract LenderCommitmentForwarder is TellerV2MarketForwarder {
     ) internal {
         for (uint256 i = 0; i < _borrowerArray.length; i++) {
             commitmentBorrowersList[_commitmentId].add(_borrowerArray[i]);
+        }
+        if (_borrowerArray.length > 0) {
+            emit UpdatedCommitmentBorrowers(_commitmentId);
         }
     }
 

95a7a7785e29a84c49a50f2f7d33654feeb9902f
Noah Passalacqua <noah@teller.org>
2023-02-23 11:21:01 -0600

    Update packages/contracts/contracts/LenderCommitmentForwarder.sol

--- packages/contracts/contracts/LenderCommitmentForwarder.sol
+++ packages/contracts/contracts/LenderCommitmentForwarder.sol
@@ -256,7 +256,7 @@ contract LenderCommitmentForwarder is TellerV2MarketForwarder {
     ) internal {
         for (uint256 i = 0; i < _borrowerArray.length; i++) {
             commitmentBorrowersList[_commitmentId].add(_borrowerArray[i]);
-        }  
+        }
         emit UpdatedCommitmentBorrowers(_commitmentId);
     }
 

e6e7f6fe221ada33bcf6156f01425e06694a28c7
Admazzola <admazzola@gmail.com>
2023-03-02 15:52:20 -0500

    switching to allocationId model

--- packages/contracts/contracts/MarketLiquidityRewards.sol
+++ packages/contracts/contracts/MarketLiquidityRewards.sol
@@ -12,6 +12,21 @@ pragma solidity ^0.8.0;
 This contract could give out 1 OHM when someone takes out a loan (for every 1000 USDC)
 
 
+    ryan ideas : 
+
+
+    thinking through conditionals we’d add to liquidity mining
+the claimer could be the lender or borrower
+ie we might be incentivizing one or the other, or both  (or some other address? idk a use case yet tho)
+principalTokenAddress
+ie the loan had to be made in USDC or x token
+collateralTokenAddress
+ie Olympus wants to incentivize holders to lock up gOHM as collateral
+maxPrincipalPerCollateral
+ie we might incentivize a collateral ratio greater than or less than some number. or this might be blank
+
+
+
 */
  
 contract MarketLiquidityRewards is 
@@ -20,23 +35,35 @@ InitializableUpgradeable
     address constant tellerV2;
     address constant marketRegistry;
 
+    uint256 allocationCount;
 
-    //marketId => rewardAllocation
+
+
+    //allocationId => rewardAllocation
     mapping(uint256 => RewardAllocation) public allocatedRewards;
 
-    //bidId => rewardWasClaimed 
-    mapping(uint256 => bool) public rewardClaimedForBid;
+    //bidId => allocationId => rewardWasClaimed 
+    mapping(uint256 => mapping(uint256 =>bool) ) public rewardClaimedForBid;
 
     struct RewardAllocation {
-        address tokenAddress;
-        uint256 tokenAmount;
+        address allocator;
+        uint256 marketId;
+        address rewardTokenAddress;
+        uint256 rewardTokenAmount;
+
+
+        //parameters for loan which affect claimability 
+        address requiredPrincipalTokenAddress; //0 for any 
+        address requiredCollateralTokenAddress; //0 for any 
+
+        uint256 rewardPerLoanPrincipalAmount; 
+       
     } 
 
     modifier onlyMarketOwner(uint256 _marketId){
         require(msg.sender == IMarketRegistry(marketRegistry).getMarketOwner(_marketId), "Only market owner can call this function.");
-    _;
-}
-
+        _;
+    }
 
     constructor(address _tellerV2, address _marketRegistry) {
         tellerV2 = _tellerV2;
@@ -48,40 +75,54 @@ InitializableUpgradeable
     }
 
 
-    function allocateRewardsForMarket(
+    function allocateRewards(
         uint256 _marketId,
         address _tokenAddress,
         uint256 _tokenAmount 
-    ) onlyMarketOwner(_marketId) external { 
-
-        require( allocatedRewards[_marketId].tokenAddress == address(0) || allocatedRewards[_marketId].tokenAddress == _tokenAddress, "Cannot change allocated token address.");
+    ) external { 
 
         IERC20(_tokenAddress).transferFrom( _msgSender() , _tokenAmount, address(this) );
 
-         allocatedRewards[_marketId] = RewardAllocation({
-            tokenAddress: address(_tokenAddress),
-            tokenAmount: ( allocatedRewards[_marketId].tokenAmount ) + _tokenAmount  
+         allocatedRewards[allocationCount++] = RewardAllocation({
+            allocator: _msgSender(),
+            marketId: _marketId,
+            rewardTokenAddress: address(_tokenAddress),
+            rewardTokenAmount:  _tokenAmount  
         });
 
+             //emit event 
     }
 
-    function deallocateRewardsForMarket(
-        uint256 _marketId
-    ) onlyMarketOwner(_marketId) external {
+    function increaseAllocationAmount(
+        uint256 _allocationId,
+        uint256 _tokenAmount 
+    ) external {
 
-        IERC20(allocatedRewards[_marketId].tokenAddress).transfer( _msgSender() , allocatedRewards[_marketId].tokenAmount );
+        IERC20(allocatedRewards[_allocationId].rewardTokenAddress).transferFrom(_msgSender(),address(this),_tokenAmount);
+        allocatedRewards[_allocationId].rewardTokenAmount += _tokenAmount;
 
-        allocatedRewards[_marketId] = RewardAllocation({
-            tokenAddress: address(0),
-            tokenAmount: 0
-        });
+        //emit event 
+    }
 
+    function deallocateRewards(
+        uint256 _allocationId
+    ) external {
 
+        require(_msgSender() == allocatedRewards[_marketId].allocator);
+
+        IERC20(allocatedRewards[_allocationId].rewardTokenAddress).transfer( _msgSender() , allocatedRewards[_marketId].rewardTokenAmount );
+
+        allocatedRewards[_allocationId].rewardTokenAmount = 0;
+
+        delete allocatedRewards[_allocationId];
+
+        //emit event 
     }
 
 
     function claimRewards(
-        uint256 _bidId
+        uint256 _allocationId,
+        uint256 _bidId       
     ) external {
 
          
@@ -92,29 +133,32 @@ InitializableUpgradeable
 
         uint256 marketId = loanData.marketId;
         address borrower = loanData.borrower;
-        uint256 rewardAmount = _calculateRewardAmount(loanData.principal);
+        uint256 amountToReward = _calculateRewardAmount(loanData.principal);
 
         //require that msgsender is the loan borrower 
         require(_msgSender() == borrower, "Only the borrower can claim reward.");
 
+        require(marketId == allocatedRewards[_allocationId].marketId, "MarketId mismatch for allocation");
 
         //transfer tokens reward to the msgsender 
-        IERC20(allocatedRewards[marketId].tokenAddress).transfer(_msgSender(), rewardAmount);
+        IERC20(allocatedRewards[_allocationId].tokenAddress).transfer(_msgSender(), amountToReward);
 
-        _decrementAllocatedAmount(marketId,rewardAmount);
+        _decrementAllocatedAmount(_allocationId,amountToReward);
+
+        //emit event 
 
     }   
 
-    function _decrementAllocatedAmount(uint256 _marketId, uint256 _rewardAmount) internal {
-        allocatedRewards[_marketId].tokenAmount -= _rewardAmount;
+    function _decrementAllocatedAmount(uint256 _allocationId, uint256 _amount) internal {
+        allocatedRewards[_allocationId].tokenAmount -= _amount;
     }
 
  
-    function _calculateRewardAmount(uint256 _loanAmount) internal view returns (uint256) {
+    function _calculateRewardAmount(uint256 _loanPrincipal) internal view returns (uint256) {
         
         //change calc -- maybe based on something set by the market owner in teh struct 
        
-        return _loanAmount / 1000;
+        return _loanPrincipal / 1000;
     }
 
 

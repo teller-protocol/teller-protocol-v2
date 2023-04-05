6584862df2feca324c22a77f00fa0cd9e4b6891f
andy <andrew@teller.org>
2023-04-05 13:43:30 -0400

    coverage image

--- packages/contracts/contracts/LenderCommitmentForwarder.sol
+++ packages/contracts/contracts/LenderCommitmentForwarder.sol
@@ -14,7 +14,6 @@ import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableSetUpgradeab
 import { MathUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
 
-import "lib/forge-std/src/console.sol";
 
 
 contract LenderCommitmentForwarder is TellerV2MarketForwarder {
@@ -348,12 +347,7 @@ contract LenderCommitmentForwarder is TellerV2MarketForwarder {
             commitment.collateralTokenAddress,
             commitment.principalTokenAddress
         );
-
-
-
-        console.logUint(requiredCollateral); 
-        console.logUint(_collateralAmount);
-
+ 
 
         if (_collateralAmount < requiredCollateral) {
             revert InsufficientBorrowerCollateral({

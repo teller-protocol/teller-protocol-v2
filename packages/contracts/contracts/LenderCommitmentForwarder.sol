b394d2c1fe8afae7554f32073a2a98bd4c14ffc8
Admazzola <admazzola@gmail.com>
2023-05-03 15:30:15 -0400

    restrict creator

--- packages/contracts/contracts/LenderCommitmentForwarder.sol
+++ packages/contracts/contracts/LenderCommitmentForwarder.sol
@@ -211,6 +211,12 @@ contract LenderCommitmentForwarder is TellerV2MarketForwarder {
         uint256 _commitmentId,
         Commitment calldata _commitment
     ) public commitmentLender(_commitmentId) {
+
+        require(
+            _commitment.lender == _msgSender(),
+            "Commitment lender cannot be updated."
+        );
+
         require(
             _commitment.principalTokenAddress ==
                 commitments[_commitmentId].principalTokenAddress,

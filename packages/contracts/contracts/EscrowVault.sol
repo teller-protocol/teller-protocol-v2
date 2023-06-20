22a3c833fa9b25837f68eddc108d88da64d7e901
Admazzola <admazzola@gmail.com>
2023-06-20 15:52:30 -0400

    spacing

--- packages/contracts/contracts/EscrowVault.sol
+++ packages/contracts/contracts/EscrowVault.sol
@@ -2,22 +2,14 @@ pragma solidity >=0.8.0 <0.9.0;
 // SPDX-License-Identifier: MIT
 
 /*
-
-
 An escrow vault for repayments 
-
-
 */
 
 // Contracts
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
-
-
-
 
 // Interfaces
 import "./interfaces/IEscrowVault.sol";

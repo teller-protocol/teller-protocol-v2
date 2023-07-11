d9b2dd303f143054272dcfeca69005704f2b4a8a
Noah Passalacqua <noah@teller.org>
2023-07-11 14:27:23 -0500

    add version strategy to build:deploy script

--- packages/subgraph/scripts/build_deploy.sh
+++ packages/subgraph/scripts/build_deploy.sh
@@ -4,10 +4,12 @@ path=$(dirname "$0")
 
 # First parameter defines the name of the network to use
 network=$1
+# Second parameter defines the version strategy to deploy
+version_strategy=$2
 
 # Ensure a network name was given
 if [ -z "$network" ]; then
   read -r -p 'Network name: ' network
 fi
 
-"$path"/build.sh "$network" && "$path"/deploy.sh "$network"
+"$path"/build.sh "$network" && "$path"/deploy.sh "$network" "$version_strategy"

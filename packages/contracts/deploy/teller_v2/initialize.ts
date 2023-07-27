86fa6ac5e4b9fa2aebfbbcac4d0b288877d3028c
Noah Passalacqua <noah@teller.org>
2023-07-27 12:09:31 -0500

    helper function to log tx url on explorer

--- packages/contracts/deploy/teller_v2/initialize.ts
+++ packages/contracts/deploy/teller_v2/initialize.ts
@@ -1,6 +1,5 @@
-import { getCurrentChainConfig } from '@nomicfoundation/hardhat-verify/dist/src/chain-config'
 import { DeployFunction } from 'hardhat-deploy/dist/types'
-import { HARDHAT_NETWORK_NAME } from 'hardhat/plugins'
+import { logTxLink } from 'helpers/logTxLink'
 import { TellerV2 } from 'types/typechain'
 
 const deployFn: DeployFunction = async (hre) => {
@@ -30,17 +29,8 @@ const deployFn: DeployFunction = async (hre) => {
     escrowVault
   )
 
-  let txLink = tx.hash
-  if (hre.network.name !== HARDHAT_NETWORK_NAME) {
-    const chainConfig = await getCurrentChainConfig(
-      hre.network,
-      hre.config.etherscan.customChains
-    )
-    txLink = `${chainConfig.urls.browserURL}/tx/${tx.hash}`
-  }
-
   hre.log('done.')
-  hre.log(`${txLink}`, { star: true, indent: 1 })
+  await logTxLink(hre, tx.hash)
   hre.log('')
   hre.log('----------')
 

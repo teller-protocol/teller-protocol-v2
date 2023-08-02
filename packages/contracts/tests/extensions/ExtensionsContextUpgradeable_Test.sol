2889eba597bbf1bdae06361018e0d3eb530e06aa
Admazzola <admazzola@gmail.com>
2023-08-02 08:00:58 -0400

    adding unit tests for extensions context

---
+++ packages/contracts/tests/extensions/ExtensionsContextUpgradeable_Test.sol
@@ -0,0 +1,93 @@
+
+
+import { Testable } from "../Testable.sol";
+
+import {ExtensionsContextUpgradeable} from "../../contracts/utils/ExtensionsContextUpgradeable.sol";
+
+import { User } from "../Test_Helpers.sol";
+
+
+contract ExtensionsContextMock is ExtensionsContextUpgradeable {
+
+    function addExtension(address extension) public {
+
+        super._addExtension(extension);
+    }
+
+    function removeExtension(address extension) public {
+
+        super._removeExtension(extension);
+    }
+
+
+}
+
+
+contract ExtensionsContext_Test is Testable {
+
+
+constructor(){}
+
+
+User private borrower;
+User private lender;
+
+ExtensionsContextMock extensionsContext;
+
+function setUp() public {
+
+    extensionsContext = new ExtensionsContextMock();
+
+
+}
+ 
+function test_addExtension() public {
+
+    extensionsContext.addExtension( address(borrower) );
+ 
+}
+
+
+
+function test_removeExtension() public {
+
+    vm.expectRevert("ExtensionsContextUpgradeable: extension not added");
+    extensionsContext.removeExtension( address(borrower) );
+
+
+}
+
+
+function test_addRemoveExtension() public {
+
+    bool isTrustedBefore = extensionsContext.isTrustedForwarder( address(borrower) );
+
+    extensionsContext.addExtension( address(borrower) );
+    extensionsContext.removeExtension( address(borrower) );
+
+    bool isTrustedAfter = extensionsContext.isTrustedForwarder( address(borrower) );
+
+  assertFalse(isTrustedBefore,"Should not be trusted forwarder");
+  assertFalse(isTrustedAfter,"Should not be trusted forwarder");
+
+}
+
+
+function test_isTrustedForwarder() public {
+
+  bool isTrustedBefore = extensionsContext.isTrustedForwarder( address(borrower) );
+
+  extensionsContext.addExtension( address(borrower) );
+  
+  bool isTrustedAfter = extensionsContext.isTrustedForwarder( address(borrower) );
+
+  assertFalse(isTrustedBefore,"Should not be trusted forwarder");
+  assertTrue(isTrustedAfter,"Should be trusted forwarder");
+
+
+}
+
+
+
+
+}

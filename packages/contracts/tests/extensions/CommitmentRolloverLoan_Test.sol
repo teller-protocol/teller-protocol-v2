2ed4d62d795fe35c47b04779892b0f7f9c1b9810
Admazzola <admazzola@gmail.com>
2023-08-06 18:47:45 -0400

    rollover scenario

--- packages/contracts/tests/extensions/CommitmentRolloverLoan_Test.sol
+++ packages/contracts/tests/extensions/CommitmentRolloverLoan_Test.sol
@@ -147,24 +147,26 @@ contract CommitmentRolloverLoan_Test is Testable {
     }*/
   
 
+  /*
+    scenario A - user needs to pay 0.1weth + 1 weth to the lender. they will get 0.5weth - 0.05 weth = 0.45 weth from the rollover to paybackthe user.  rest 0.65 needs to be paid back by the borrower.abi
+    Scenario B - user needs to pay 0.1weth + 1 weth back to the lender. They will get 1.2weth - 0.12weth = 1.08 weth from the rollover to pay back the user so 0.02 needs to be paid back to the borrower.abi
+    Scenario C - user needs to pay 0.1 weth + 1 weth back to the lender.  They will get 2 weth - 0.2 weth = 1.8weth so 0.6 weth is given to the borrower .  
+
+    assume that 10 pct fee is taken by pool plus protocol for simplicity. 
+
+
+
+  */
+
   function test_rolloverLoan_financial_scenario_A() public {
 
     address lendingToken = address(wethMock);
          uint256 marketId = 0;
          uint256 principalAmount = 500;
-         uint32 duration = 10 days;
+         uint32 duration = 36 days;
          uint16 interestRate = 100;
 
-         ICommitmentRolloverLoan.AcceptCommitmentArgs memory commitmentArgs = ICommitmentRolloverLoan.AcceptCommitmentArgs({
-            commitmentId: 0,
-            principalAmount: principalAmount,
-            collateralAmount: 100,
-            collateralTokenId: 0,
-            collateralTokenAddress: address(0),
-            interestRate: interestRate,
-            loanDuration: duration
-         });
-
+         
         vm.prank(address(borrower));
         uint256 loanId = tellerV2.submitBid( 
             lendingToken,
@@ -180,11 +182,31 @@ contract CommitmentRolloverLoan_Test is Testable {
         wethMock.approve(address(tellerV2),1e18);
 
         vm.prank(address(lender));
-        (uint256 loanId) = tellerV2.lenderAcceptBid( 
+        (uint256 amountToProtocol,uint256 amountToMarketplace, uint256 amountToBorrower) = tellerV2.lenderAcceptBid( 
             loanId  
          );
 
+        vm.warp(36 days); 
 
+
+        ICommitmentRolloverLoan.AcceptCommitmentArgs memory commitmentArgs = ICommitmentRolloverLoan.AcceptCommitmentArgs({
+            commitmentId: 0,
+            principalAmount: principalAmount,
+            collateralAmount: 100,
+            collateralTokenId: 0,
+            collateralTokenAddress: address(0),
+            interestRate: interestRate,
+            loanDuration: duration
+         });
+
+
+        vm.prank(address(borrower));
+        
+        commitmentRolloverLoan.rolloverLoan(
+            loanId,
+            commitmentArgs
+        );
+ 
         
 
 

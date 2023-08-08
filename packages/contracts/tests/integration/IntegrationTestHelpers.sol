c41177db4d010cc6efd39d1a22765e8d5258fd30
Admazzola <admazzola@gmail.com>
2023-08-08 09:50:10 -0400

    finished solidity deployscript for integration tests

--- packages/contracts/tests/integration/IntegrationTestHelpers.sol
+++ packages/contracts/tests/integration/IntegrationTestHelpers.sol
@@ -7,17 +7,39 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import { TellerV2 } from "../../contracts/TellerV2.sol";
 
-/*import "../contracts/mock/WethMock.sol";
-import "../contracts/interfaces/IMarketRegistry.sol";
-import "../contracts/interfaces/ITellerV2.sol";
-import "../contracts/interfaces/ITellerV2Context.sol";
-import { Collateral } from "../contracts/interfaces/escrow/ICollateralEscrowV1.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { PaymentType } from "../contracts/libraries/V2Calculations.sol";
-*/
+
+
+import "../../contracts/EAS/TellerAS.sol";
+import "../../contracts/EAS/TellerASEIP712Verifier.sol";
+import "../../contracts/EAS/TellerASRegistry.sol";
+
+import {MarketRegistry} from  "../../contracts/MarketRegistry.sol";
+import {EscrowVault} from  "../../contracts/EscrowVault.sol";
+import {LenderManager} from "../../contracts/LenderManager.sol";
+import {LenderCommitmentForwarder} from  "../../contracts/LenderCommitmentForwarder.sol";
+import {CollateralManager} from "../../contracts/CollateralManager.sol";
+import {CollateralEscrowV1}  from "../../contracts/escrow/CollateralEscrowV1.sol";
+ 
+import {ReputationManager} from  "../../contracts/ReputationManager.sol";
+import {IMarketRegistry} from  "../../contracts/interfaces/IMarketRegistry.sol";
+
+
 library IntegrationTestHelpers {
 
 
+
+function deployMarketRegistry() public returns (address) {
+
+    IASRegistry iasRegistry = new TellerASRegistry();
+    IEASEIP712Verifier ieaseip712verifier = new TellerASEIP712Verifier();
+
+    TellerAS tellerAS = new TellerAS( (iasRegistry), (ieaseip712verifier));
+    MarketRegistry marketRegistry = new MarketRegistry();
+
+    marketRegistry.initialize(tellerAS);
+
+    return address(marketRegistry);
+}
 
 function deployIntegrationSuite() public returns (address tellerV2_){
 
@@ -25,22 +47,27 @@ function deployIntegrationSuite() public returns (address tellerV2_){
     TellerV2 tellerV2 = new TellerV2(trustedForwarder);
 
         uint16 _protocolFee = 10;
-        address _marketRegistry,
-        address _reputationManager,
-        address _lenderCommitmentForwarder,
-        address _collateralManager,
-        address _lenderManager,
-        address _escrowVault
+        address _marketRegistry = deployMarketRegistry();
+        ReputationManager _reputationManager = new ReputationManager();
+        LenderCommitmentForwarder _lenderCommitmentForwarder = new LenderCommitmentForwarder(address(tellerV2), address(_marketRegistry));
+        CollateralManager _collateralManager = new CollateralManager();
+        LenderManager _lenderManager = new LenderManager(IMarketRegistry(_marketRegistry));
+        EscrowVault _escrowVault = new EscrowVault();
 
+        CollateralEscrowV1 collateralEscrowBeacon = new CollateralEscrowV1();
+    _collateralManager.initialize(address(collateralEscrowBeacon),address(tellerV2));
+    _lenderManager.initialize();
+    _reputationManager.initialize(address(tellerV2));
+    
     tellerV2.initialize(
 
          _protocolFee,
-         _marketRegistry,
-         _reputationManager,
-         _lenderCommitmentForwarder,
-         _collateralManager,
-         _lenderManager,
-         _escrowVault
+         address(_marketRegistry),
+         address(_reputationManager),
+         address(_lenderCommitmentForwarder),
+         address(_collateralManager),
+         address(_lenderManager),
+         address( _escrowVault)
     );
 
 }

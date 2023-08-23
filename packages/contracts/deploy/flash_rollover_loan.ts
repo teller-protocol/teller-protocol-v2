b80a0a3eb884fc994c8b3c91dd51e650f2a32163
andy <admazzola@gmail.com>
2023-08-23 15:30:17 -0400

    update deploy script

--- packages/contracts/deploy/flash_rollover_loan.ts
+++ packages/contracts/deploy/flash_rollover_loan.ts
@@ -5,13 +5,23 @@ const deployFn: DeployFunction = async (hre) => {
   const lenderCommitmentForwarder = await hre.contracts.get(
     'LenderCommitmentForwarder'
   )
-  const commitmentRolloverLoan = await hre.deployProxy(
-    'CommitmentRolloverLoan',
+
+  //fix me 
+  const networkName = "goerli";
+
+  const aavePoolAddressProvider:  { [networkName: string]: string }  = { 
+    "mainnet": "0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e",
+    "goerli": "0x73D94B5D5C0a68Fe279a91b23D2165D2DAaA41d3"  
+  }; 
+
+  const flashRolloverLoan = await hre.deployProxy(
+    'FlashRolloverLoan',
     {
       unsafeAllow: ['constructor', 'state-variable-immutable'],
       constructorArgs: [
         await tellerV2.getAddress(),
-        await lenderCommitmentForwarder.getAddress()
+        await lenderCommitmentForwarder.getAddress(),
+        aavePoolAddressProvider[networkName]
       ]
     }
   )
@@ -20,8 +30,8 @@ const deployFn: DeployFunction = async (hre) => {
 }
 
 // tags and deployment
-deployFn.id = 'commitment-rollover-loan:deploy'
-deployFn.tags = ['commitment-rollover-loan', 'commitment-rollover-loan:deploy']
+deployFn.id = 'flash-rollover-loan:deploy'
+deployFn.tags = ['flash-rollover-loan', 'flash-rollover-loan:deploy']
 deployFn.dependencies = [
   'teller-v2:deploy',
   'lender-commitment-forwarder:deploy'

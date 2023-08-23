eb4df4c798ea5b013a80725e04f06ab3ecd3e063
andy <admazzola@gmail.com>
2023-08-23 15:45:07 -0400

    improve deploy script

--- packages/contracts/deploy/flash_rollover_loan.ts
+++ packages/contracts/deploy/flash_rollover_loan.ts
@@ -1,18 +1,23 @@
 import { DeployFunction } from 'hardhat-deploy/dist/types'
 
+
+//feel free to add more ..
+const aavePoolAddressProvider:  { [networkName: string]: string }  = { 
+  "mainnet": "0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e",
+  "goerli": "0x73D94B5D5C0a68Fe279a91b23D2165D2DAaA41d3"  
+}; 
+
+const networksWithAave: string[] = Object.keys(aavePoolAddressProvider);
+
+
 const deployFn: DeployFunction = async (hre) => {
   const tellerV2 = await hre.contracts.get('TellerV2')
   const lenderCommitmentForwarder = await hre.contracts.get(
     'LenderCommitmentForwarder'
   )
 
-  //fix me 
-  const networkName = "goerli";
-
-  const aavePoolAddressProvider:  { [networkName: string]: string }  = { 
-    "mainnet": "0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e",
-    "goerli": "0x73D94B5D5C0a68Fe279a91b23D2165D2DAaA41d3"  
-  }; 
+ 
+  const networkName = hre.network.name;
 
   const flashRolloverLoan = await hre.deployProxy(
     'FlashRolloverLoan',
@@ -36,4 +41,11 @@ deployFn.dependencies = [
   'teller-v2:deploy',
   'lender-commitment-forwarder:deploy'
 ]
+
+deployFn.skip = async (hre) => {
+  return (
+    !hre.network.live ||
+    !networksWithAave.includes(hre.network.name)
+  )
+}
 export default deployFn

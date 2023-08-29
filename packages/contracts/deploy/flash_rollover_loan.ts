c81bff3321d799296e32d2ffdf3442ec4ab38f72
Admazzola <admazzola@gmail.com>
2023-08-29 13:37:50 -0400

    add sepolia

--- packages/contracts/deploy/flash_rollover_loan.ts
+++ packages/contracts/deploy/flash_rollover_loan.ts
@@ -4,7 +4,8 @@ import { DeployFunction } from 'hardhat-deploy/dist/types'
 const aavePoolAddressProvider: { [networkName: string]: string } = {
   mainnet: '0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e',
   goerli: '0xC911B590248d127aD18546B186cC6B324e99F02c',
-  sepolia: '0x0496275d34753A48320CA58103d5220d394FF77F'
+  sepolia: '0x0496275d34753A48320CA58103d5220d394FF77F',
+  polygon: '0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb'
 }
 
 const networksWithAave: string[] = Object.keys(aavePoolAddressProvider)

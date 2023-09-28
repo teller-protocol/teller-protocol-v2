259a6585402c895997ec53441891f4490707f77e
andy <admazzola@gmail.com>
2023-09-28 13:01:18 -0400

    fixed scripts

--- packages/contracts/deploy/upgrades/03_flash_rollover_g3.ts
+++ packages/contracts/deploy/upgrades/03_flash_rollover_g3.ts
@@ -1,14 +1,30 @@
 import { DeployFunction } from 'hardhat-deploy/dist/types'
 
+
+
+const aavePoolAddressProvider: { [networkName: string]: string } = {
+  mainnet: '0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e',
+  goerli: '0xC911B590248d127aD18546B186cC6B324e99F02c',
+  sepolia: '0x0496275d34753A48320CA58103d5220d394FF77F',
+  polygon: '0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb',
+  arbitrum: '0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb',
+  base: '0xe20fCBdBfFC4Dd138cE8b2E6FBb6CB49777ad64D',
+}
+const networksWithAave: string[] = Object.keys(aavePoolAddressProvider)
+
+
 const deployFn: DeployFunction = async (hre) => {
   hre.log('----------')
   hre.log('')
   hre.log('FlashRolloverLoan G3: Proposing upgrade...')
 
+  const networkName = hre.network.name
+
+  const flashRolloverLoan = await hre.contracts.get('FlashRolloverLoan')
   const tellerV2 = await hre.contracts.get('TellerV2')
   const marketRegistry = await hre.contracts.get('MarketRegistry')
-  const lenderCommitmentForwarder = await hre.contracts.get(
-    'FlashRolloverLoan'
+  const LenderCommitmentForwarderStaging = await hre.contracts.get(
+    'LenderCommitmentForwarderStaging'
   )
 
   await hre.defender.proposeBatchTimelock({
@@ -20,7 +36,7 @@ const deployFn: DeployFunction = async (hre) => {
 `,
     _steps: [
       {
-        proxy: lenderCommitmentForwarder,
+        proxy: flashRolloverLoan,
         implFactory: await hre.ethers.getContractFactory(
           'FlashRolloverLoan'
         ),
@@ -29,7 +45,8 @@ const deployFn: DeployFunction = async (hre) => {
           unsafeAllow: ['constructor', 'state-variable-immutable'],
           constructorArgs: [
             await tellerV2.getAddress(),
-            await marketRegistry.getAddress(),
+            await LenderCommitmentForwarderStaging.getAddress(),
+            aavePoolAddressProvider[networkName],
           ],
         },
       },

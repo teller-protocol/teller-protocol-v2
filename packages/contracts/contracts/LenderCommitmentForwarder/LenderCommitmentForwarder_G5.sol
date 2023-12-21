269c8812aff058c84ff668166a487e262009341b
Admazzola <admazzola@gmail.com>
2023-12-21 11:41:36 -0500

    adding method to LCF

---
+++ packages/contracts/contracts/LenderCommitmentForwarder/LenderCommitmentForwarder_G5.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.8.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+
+// Contracts
+import "./LenderCommitmentForwarder_G4.sol";
+import "./extensions/ExtensionsContextUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract LenderCommitmentForwarder_G5 is LenderCommitmentForwarder_G4 {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor(address _tellerV2, address _marketRegistry)
+        LenderCommitmentForwarder_G4(_tellerV2, _marketRegistry)
+    {}
+
+    function getCommitmentCollateralTokenType(
+        uint256 _commitmentId
+        
+    ) public view returns (CommitmentCollateralType) {
+       return commitments[_commitmentId].collateralTokenType;
+    } 
+
+}

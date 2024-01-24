0a12eaa4e052db48ee08ac73384e6d80805908ec
Admazzola <admazzola@gmail.com>
2024-01-24 12:49:19 -0500

    deploy lcf alpha

--- packages/contracts/deploy/lender_commitment_forwarder/deploy_alpha.ts
+++ packages/contracts/deploy/lender_commitment_forwarder/deploy_alpha.ts
@@ -4,13 +4,34 @@ const deployFn: DeployFunction = async (hre) => {
   const tellerV2 = await hre.contracts.get('TellerV2')
   const marketRegistry = await hre.contracts.get('MarketRegistry')
 
+  let uniswapFactoryAddress: string
+  switch (hre.network.name) {
+    case 'mainnet':
+    case 'goerli':
+    case 'arbitrum':
+    case 'optimism':
+    case 'polygon':
+    case 'localhost':
+      uniswapFactoryAddress = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+      break
+    case 'base':
+      uniswapFactoryAddress = '0x33128a8fC17869897dcE68Ed026d694621f6FDfD'
+      break
+    case 'sepolia':
+      uniswapFactoryAddress = '0x0227628f3F023bb0B980b67D528571c95c6DaC1c'
+      break
+    default:
+      throw new Error('No swap factory address found for this network')
+  }
+
   const lenderCommitmentForwarderAlpha = await hre.deployProxy(
     'LenderCommitmentForwarderAlpha',
     {
       unsafeAllow: ['constructor', 'state-variable-immutable'],
       constructorArgs: [
         await tellerV2.getAddress(),
-        await marketRegistry.getAddress()
+        await marketRegistry.getAddress(),
+        uniswapFactoryAddress
       ]
     }
   )

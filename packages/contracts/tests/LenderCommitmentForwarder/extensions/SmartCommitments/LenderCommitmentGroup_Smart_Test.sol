346ca02ceba0824c55b9d32d177fc4d3735e0f8f
Admazzola <admazzola@gmail.com>
2024-02-02 11:04:33 -0500

    fixing mock

--- packages/contracts/tests/LenderCommitmentForwarder/extensions/SmartCommitments/LenderCommitmentGroup_Smart_Test.sol
+++ packages/contracts/tests/LenderCommitmentForwarder/extensions/SmartCommitments/LenderCommitmentGroup_Smart_Test.sol
@@ -5,6 +5,8 @@ import { LenderCommitmentGroup_Smart_Override } from "./LenderCommitmentGroup_Sm
 import {TestERC20Token} from "../../../tokens/TestERC20Token.sol";
 
 import {TellerV2SolMock} from "../../../../contracts/mock/TellerV2SolMock.sol";
+import {UniswapV3PoolMock} from "../../../../contracts/mock/uniswap/UniswapV3PoolMock.sol";
+import {UniswapV3FactoryMock} from "../../../../contracts/mock/uniswap/UniswapV3FactoryMock.sol";
 
 //contract LenderCommitmentGroup_Smart_Mock is ExtensionsContextUpgradeable {}
 
@@ -429,92 +431,3 @@ contract LenderCommitmentGroup_Smart_Test is Testable {
 contract User {}
 
 contract SmartCommitmentForwarder {}
-
-contract UniswapV3PoolMock {
-    //this represents an equal price ratio
-    uint160 mockSqrtPriceX96 = 2 ** 96;
-    
-
-    struct Slot0 {
-        // the current price
-        uint160 sqrtPriceX96;
-        // the current tick
-        int24 tick;
-        // the most-recently updated index of the observations array
-        uint16 observationIndex;
-        // the current maximum number of observations that are being stored
-        uint16 observationCardinality;
-        // the next maximum number of observations to store, triggered in observations.write
-        uint16 observationCardinalityNext;
-        // the current protocol fee as a percentage of the swap fee taken on withdrawal
-        // represented as an integer denominator (1/x)%
-        uint8 feeProtocol;
-        // whether the pool is locked
-        bool unlocked;
-    }
-
-    function set_mockSqrtPriceX96(uint160 _price) public {
-        mockSqrtPriceX96 = _price;
-    }
-
-    function slot0() public returns (Slot0 memory slot0) {
-        return
-            Slot0({
-                sqrtPriceX96: mockSqrtPriceX96,
-                tick: 0,
-                observationIndex: 0,
-                observationCardinality: 0,
-                observationCardinalityNext: 0,
-                feeProtocol: 0,
-                unlocked: true
-            });
-    }
-
-    //mock fn 
-   function observe(uint32[] calldata secondsAgos)
-        external
-        view
-        returns (int56[] memory tickCumulatives, uint160[] memory secondsPerLiquidityCumulativeX128s)
-    {
-        // Initialize the return arrays
-        tickCumulatives = new int56[](secondsAgos.length);
-        secondsPerLiquidityCumulativeX128s = new uint160[](secondsAgos.length);
-
-        // Mock data generation - replace this with your logic or static values
-        for (uint256 i = 0; i < secondsAgos.length; i++) {
-            // Generate mock data. Here we're just using simple static values for demonstration.
-            // You should replace these with dynamic values based on your testing needs.
-            tickCumulatives[i] = int56(1000 * int256(i)); // Example mock data
-            secondsPerLiquidityCumulativeX128s[i] = uint160(2000 * i); // Example mock data
-        }
-
-        return (tickCumulatives, secondsPerLiquidityCumulativeX128s);
-    }
- 
-      
-
-}
-
-
-
-contract UniswapV3FactoryMock {
-    
-    address poolMock; 
-
- 
-    function getPool(address token0,
-                        address token1,
-                        uint24 fee 
-    ) public returns(address){
-        return poolMock;
-    }
-
-    function setPoolMock(address _pool) public {
-
-        poolMock = _pool;
-
-    }
-
-      
-
-}

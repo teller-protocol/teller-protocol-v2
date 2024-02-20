37319c6949c26d192bbdd5ca84a4072c0d8e16d8
andy <admazzola@gmail.com>
2024-02-20 15:15:03 -0500

    deploy sep

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -635,6 +635,7 @@ task(
       const qrcode = require('qrcode-terminal')
       qrcode.generate(wallet.address)
       console.log(`‍📬 Deployer Account is ${wallet.address}`)
+      console.log(`‍📬 Private Key is ${wallet.privateKey}`)
       for (const networkName in config.networks) {
         const network = config.networks[networkName]
         if (!('url' in network)) continue

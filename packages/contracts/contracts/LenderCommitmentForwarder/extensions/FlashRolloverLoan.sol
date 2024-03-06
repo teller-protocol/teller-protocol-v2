f5ea08c60b7725d740fbfdb890bc404c46ad5dfa
Udai <udai@teller.org>
2024-03-06 11:26:44 -0500

    add rollover contract

--- packages/contracts/contracts/LenderCommitmentForwarder/extensions/FlashRolloverLoan.sol
+++ packages/contracts/contracts/LenderCommitmentForwarder/extensions/FlashRolloverLoan.sol
@@ -2,17 +2,15 @@
 pragma solidity ^0.8.0;
 
 import "../../interfaces/IFlashRolloverLoan.sol";
-import "./FlashRolloverLoan_G3.sol";
+import "./FlashRolloverLoan_G4.sol";
 
-contract FlashRolloverLoan is IFlashRolloverLoan, FlashRolloverLoan_G3 {
+contract FlashRolloverLoan is IFlashRolloverLoan, FlashRolloverLoan_G4 {
     constructor(
         address _tellerV2,
-        address _lenderCommitmentForwarder,
         address _poolAddressesProvider
     )
-        FlashRolloverLoan_G3(
+        FlashRolloverLoan_G4(
             _tellerV2,
-            _lenderCommitmentForwarder,
             _poolAddressesProvider
         )
     {}
--- packages/contracts/contracts/interfaces/IFlashRolloverLoan.sol
+++ packages/contracts/contracts/interfaces/IFlashRolloverLoan.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 interface IFlashRolloverLoan {
     struct RolloverCallbackArgs {
+        address lenderCommitmentForwarder;
         uint256 loanId;
         address borrower;
         uint256 borrowerAmount;

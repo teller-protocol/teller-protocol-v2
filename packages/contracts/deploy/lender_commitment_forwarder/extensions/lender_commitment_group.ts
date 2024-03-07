055e473461fed3bb2984a54a61094fc7ebd742e2
Admazzola <admazzola@gmail.com>
2024-03-07 15:42:22 -0500

    add to script

--- packages/contracts/deploy/lender_commitment_forwarder/extensions/lender_commitment_group.ts
+++ packages/contracts/deploy/lender_commitment_forwarder/extensions/lender_commitment_group.ts
@@ -11,8 +11,29 @@ const deployFn: DeployFunction = async (hre) => {
   const smartCommitmentForwarderAddress =
     await SmartCommitmentForwarder.getAddress()
 
-  //for sepolia
-  const uniswapV3FactoryAddress = '0x0227628f3F023bb0B980b67D528571c95c6DaC1c'
+  
+
+  let uniswapV3FactoryAddress: string
+  switch (hre.network.name) {
+    case 'mainnet':
+    case 'goerli':
+    case 'arbitrum':
+    case 'optimism':
+    case 'polygon':
+    case 'localhost':
+      uniswapV3FactoryAddress = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+      break
+    case 'base':
+      uniswapV3FactoryAddress = '0x33128a8fC17869897dcE68Ed026d694621f6FDfD'
+      break
+    case 'sepolia':
+      uniswapV3FactoryAddress = '0x0227628f3F023bb0B980b67D528571c95c6DaC1c'
+      break
+    default:
+      throw new Error('No swap factory address found for this network')
+  }
+
+
 
   const networkName = hre.network.name
 
@@ -64,6 +85,6 @@ deployFn.dependencies = [
 ]
 
 deployFn.skip = async (hre) => {
-  return !hre.network.live || !['sepolia'].includes(hre.network.name)
+  return !hre.network.live || !['sepolia','polygon'].includes(hre.network.name)
 }
 export default deployFn

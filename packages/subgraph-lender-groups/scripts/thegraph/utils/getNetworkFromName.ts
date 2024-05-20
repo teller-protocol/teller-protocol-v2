0673fc9e320e8718028dc829ff74b32d2448f388
Admazzola <admazzola@gmail.com>
2024-05-20 12:56:13 -0400

    add to subgraph config

--- packages/subgraph-lender-groups/scripts/thegraph/utils/getNetworkFromName.ts
+++
@@ -1,5 +0,0 @@
-export const getNetworkFromName = (name: string): string => {
-  const match = name.match(/^tellerv2-(.+)$/);
-  if (!match) throw new Error(`Invalid subgraph name: ${name}`);
-  return match[1];
-};

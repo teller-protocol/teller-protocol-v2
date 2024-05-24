07274266f8ec552ece60d22f3c7312f1d3bc3c9a
andy <admazzola@gmail.com>
2024-05-24 13:07:25 -0400

    improve mapping

--- packages/subgraph-substreamed/src/mapping.ts
+++ packages/subgraph-substreamed/src/mapping.ts
@@ -1,31 +1,39 @@
  
- import { log } from "@graphprotocol/graph-ts";
- import { fac_DeployedLenderGroupContract as DeployedLenderGroupContract } from "./pb/contract/v1/fac_DeployedLenderGroupContract";
-// import { Transaction } from "../generated/schema";
+ import { log, Address, BigInt ,Timestamp , Bytes } from "@graphprotocol/graph-ts";
+ import { Events } from "./pb/contract/v1/Events";
+
  import { Protobuf } from 'as-proto/assembly';
 
+
+ import { fac_deployed_lender_group_contract } from "../generated/schema";
  
 export function handleSubstreamGraphOutTrigger(bytes: Uint8Array): void {
   
-  const deployedLenderGroupContractProto: DeployedLenderGroupContract = Protobuf.decode<DeployedLenderGroupContract>(bytes, DeployedLenderGroupContract.decode);
+  const events: Events = Protobuf.decode<Events>(bytes, Events.decode);
   
-  log.info("Decoded a lender group contract from proto" , []);
-  //const transactions = transactionsProto.transactions;
+  log.info("Decoded an event from substream trigger" , []  );
+  
  
+  for (let i = 0; i < events.facDeployedLenderGroupContracts.length; i++) {
+    let deployedLenderGroupContractEvent = events.facDeployedLenderGroupContracts[i];
 
+    let event_id = deployedLenderGroupContractEvent.evtTxHash
+    .concat( "_" )
+    .concat( deployedLenderGroupContractEvent.evtIndex.toString() );   
+    let entity = new fac_deployed_lender_group_contract( event_id );
 
-  /* let transactions = assembly.eth.transaction.v1.Transactions.decode(bytes.buffer).transactions;
-  if (transactions.length == 0) {
-      log.info("No transactions found", []);
-      return;
-  }
+    if (deployedLenderGroupContractEvent.evtBlockTime) {
 
-  for (let i = 0; i < transactions.length; i++) {
-      let transaction = transactions[i];
-
-      let entity = new Transaction(transaction.hash);
-      entity.from = transaction.from;
-      entity.to = transaction.to;
+      entity.evt_tx_hash = deployedLenderGroupContractEvent.evtTxHash;
+      entity.evt_block_number = BigInt.fromU64( deployedLenderGroupContractEvent.evtBlockNumber );
+      
+      entity.evt_index =  BigInt.fromI32(deployedLenderGroupContractEvent.evtIndex);
+      entity.group_contract = deployedLenderGroupContractEvent.groupContract.toString();  //address .. ? 
+      
       entity.save();
-  }  */
+
+    }
+   
+} 
+ 
 }

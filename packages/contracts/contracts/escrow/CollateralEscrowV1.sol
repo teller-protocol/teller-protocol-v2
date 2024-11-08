ba6f5a97d3a0dbcdc17ddf7c2dff8fd5f89c1027
Admazzola <admazzola@gmail.com>
2024-11-08 12:35:33 -0500

    safe transfer

--- packages/contracts/contracts/escrow/CollateralEscrowV1.sol
+++ packages/contracts/contracts/escrow/CollateralEscrowV1.sol
@@ -10,6 +10,7 @@ import { SafeERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721ReceiverUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../interfaces/escrow/ICollateralEscrowV1.sol";
 
 contract CollateralEscrowV1 is OwnableUpgradeable, ICollateralEscrowV1 {
@@ -116,7 +117,10 @@ contract CollateralEscrowV1 is OwnableUpgradeable, ICollateralEscrowV1 {
             "Asset not allowed to be withdrawn as dust"
         ); 
      
-        IERC20Upgradeable(tokenAddress).transfer(recipient, amount);        
+         
+        SafeERC20Upgradeable.safeTransfer(IERC20Upgradeable(tokenAddress),recipient, amount);      
+
+       
     }
 
 
@@ -180,8 +184,8 @@ contract CollateralEscrowV1 is OwnableUpgradeable, ICollateralEscrowV1 {
         address _recipient
     ) internal {
         // Withdraw ERC20
-        if (_collateral._collateralType == CollateralType.ERC20) {
-            IERC20Upgradeable(_collateralAddress).transfer(_recipient, _amount);
+        if (_collateral._collateralType == CollateralType.ERC20) { 
+            SafeERC20Upgradeable.safeTransfer(IERC20Upgradeable(_collateralAddress),_recipient, _amount);      
         }
         // Withdraw ERC721
         else if (_collateral._collateralType == CollateralType.ERC721) {

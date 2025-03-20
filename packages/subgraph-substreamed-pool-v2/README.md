e22ba02ff94934dd0ae1425aa2d1430f69552f47
Admazzola <admazzola@gmail.com>
2025-03-20 16:01:07 -0400

    add

---
+++ packages/subgraph-substreamed-pool-v2/README.md
@@ -0,0 +1,105 @@
+### Lender Groups Substreams
+
+
+
+
+- use map-events 
+
+
+-- Uniswap Example 
+https://github.com/streamingfast/substreams-uniswap-v3/blob/develop/src/rpc.rs
+
+
+
+### recompile from contracts 
+
+
+-  how ???
+
+- need to run buf.gen.yaml ? 
+
+1. manually  regen  the /proto folder 
+
+2. substreams protogen 
+
+cargo run --bin rebuild_abi
+
+
+```
+make protogen   ??? 
+```
+
+
+
+
+### Adding a new custom output type 
+
+> Add it to contract proto ? 
+
+
+
+
+
+### Building 
+
+> run proto gen (to output RUST ) 
+
+
+```
+substreams protogen 
+```
+
+
+
+> Create a substreams spkg 
+
+```
+substreams pack ./substreams.yaml
+```
+
+
+#### DEPLOYING 
+
+
+
+1. make sure FACTORY_TRACKED_CONTRACT+COLLATERAL_MANAGER_TRACKED_CONTRACT in 'lib'  is defined properly for network 
+2. make sure data in export_build.rs is defined properly for network (see https://thegraph.com/docs/en/supported-networks/ ) 
+
+
+3. cargo run --bin exportbuild   //regenerate yaml files 
+4. make && make build && make pack 
+
+
+5. graph auth   (optional) 
+6. graph deploy   tellerv2-lender-groups-mainnet --version-label 0.4.21.120
+ 
+ 0.4.21.120 
+
+
+(  use graph deploy --studio    with old version of graph cli ) 
+
+
+### graph names 
+
+tellerv2-lender-groups-polygon  *
+tellerv2-lender-groups-arbitrum *
+tellerv2-lender-groups-base  * 
+tellerv2-lender-groups-mainnet *
+
+
+
+
+
+
+ ### CHECK THE STATUS 
+
+ https://api.studio.thegraph.com/query/36377/tellerv2-lender-groups-polygon/0.4.21.103/graphql
+
+
+
+
+
+ ### HOW THINGS WORK UNDER THE HOOD
+
+ When deploying to a subgraph, the config file  substreams.subgraph.yaml is used.  
+ When you are doing tables.create_row,  that data must match what it is in  schema.graphql ! 
---
+++ packages/subgraph-substreamed-pool-v2/config/export_build.rs
@@ -0,0 +1,155 @@
+use std::path::Path;
+use handlebars::Handlebars;
+use serde::Serialize;
+use std::collections::HashMap;
+use std::fs::{self, File};
+use std::io::{self, Read, Write};
+
+#[derive(Serialize)]
+struct Data {
+    graph_network: String,
+    chain_network: String ,
+    start_block: u32 ,
+   
+}
+
+/*
+
+Polygon
+            {
+                graph_network: "polygon".to_string(),  
+                chain_network: "matic".to_string(),    
+                start_block:    66108200 on polygon 
+          }
+
+
+Arbitrum 
+        { 
+
+          graph_network: "arbitrum-one".to_string(),  
+            chain_network: "arbitrum-one".to_string(),   
+            start_block:  292978933     
+
+        }
+
+
+
+Base 
+        { 
+
+            graph_network: "base".to_string(),  
+            chain_network: "base".to_string(),   
+            start_block:  24824400    
+
+        }
+
+
+
+
+Mainnet 
+        { 
+
+            graph_network: "mainnet".to_string(),  
+            chain_network: "mainnet".to_string(),   
+            start_block:  21616780    
+
+        }
+
+
+*/
+
+impl Default for Data {
+
+	fn default() -> Self {
+
+		Self {
+			 graph_network: "polygon".to_string(),  
+            chain_network: "matic".to_string(),   
+            start_block:  66108200    
+		}
+
+	}
+
+}
+
+
+// Function to load the template file content
+fn load_template(file_path: &str) -> io::Result<String> {
+    let mut file = File::open(file_path)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+// Function to process the template and data
+fn process_template(template: &str, data: &Data) -> String {
+    let mut handlebars = Handlebars::new();
+    let rendered = handlebars.render_template(template, &data).unwrap();
+    rendered
+}
+
+// Function to process a single file and write output to the output folder
+fn process_file(input_file: &str, output_file: &str, data: &Data) -> io::Result<()> {
+    let template = load_template(input_file)?;
+    let updated_content = process_template(&template, &data);
+
+    // Write the updated content to the output file
+    let mut file = File::create(output_file)?;
+    file.write_all(updated_content.as_bytes())?;
+    println!("File processed: {} -> {}", input_file, output_file);
+    Ok(())
+}
+
+
+fn main() -> io::Result<()> {
+    // Define the data to be injected into the template
+    let data = Data::default();
+
+
+    let input_folder = "./config/build_inputs";
+    let output_folder = "./";
+
+	println!("Input folder: {}", input_folder);
+    println!("Output folder: {}", output_folder);
+
+    // Check if input folder exists
+    if !Path::new(input_folder).exists() {
+        eprintln!("Error: Input folder '{}' does not exist.", input_folder);
+        std::process::exit(1);
+    }
+
+    // Ensure the output directory exists
+    if !Path::new(output_folder).exists() {
+        println!("Output folder does not exist. Creating it...");
+        fs::create_dir_all(output_folder).expect("Failed to create output folder");
+    }
+
+    // Process all files in the input directory
+    process_files_in_directory(input_folder, output_folder, &data)?;
+
+
+
+    Ok(())
+}
+
+
+
+fn process_files_in_directory(input_dir: &str, output_dir: &str, data: &Data) -> io::Result<()> {
+    // Read all files from the input directory
+    let entries = fs::read_dir(input_dir)?;
+
+    for entry in entries {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path.is_file() {
+            let input_file = path.to_str().unwrap();
+            let file_name = path.file_name().unwrap().to_str().unwrap();
+            let output_file = Path::new(output_dir).join(file_name);
+
+            process_file(input_file, output_file.to_str().unwrap(), data)?;
+        }
+    }
+
+    Ok(())
+}

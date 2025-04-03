634ef29195e006eb480b4147dc46c9c397b6667b
andy <admazzola@gmail.com>
2025-04-03 15:26:51 -0400

    deploy script for lcfa v2

--- packages/contracts/contracts/LenderCommitmentForwarder/LenderCommitmentForwarderV2.sol
+++ packages/contracts/contracts/LenderCommitmentForwarder/LenderCommitmentForwarderV2.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "./LenderCommitmentForwarder_U1.sol";
+import "./LenderCommitmentForwarder_U2.sol";
 
-contract LenderCommitmentForwarderAlpha is LenderCommitmentForwarder_U1 {
+contract LenderCommitmentForwarderV2 is LenderCommitmentForwarder_U2 {
     constructor(
         address _tellerV2,
         address _marketRegistry,
         address _uniswapV3Factory
     )
-        LenderCommitmentForwarder_U1(
+        LenderCommitmentForwarder_U2(
             _tellerV2,
             _marketRegistry,
             _uniswapV3Factory

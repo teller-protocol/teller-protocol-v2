0b06c330790af19c785ac0e180eaa725d471f706
andy <admazzola@gmail.com>
2025-04-15 12:40:41 -0400

    proposed upgrade to pools v2

--- packages/contracts/deploy/upgrades/25_upgrade_lender_groups_beacon_v2_redeem_time.ts
+++ packages/contracts/deploy/upgrades/25_upgrade_lender_groups_beacon_v2_redeem_time.ts
@@ -3,11 +3,11 @@ import { DeployFunction } from 'hardhat-deploy/dist/types'
 const deployFn: DeployFunction = async (hre) => {
   hre.log('----------')
   hre.log('')
-  hre.log('Lendergroups: Proposing upgrade...')
+  hre.log('Lender pools V2: Proposing upgrade...')
 
 
 
-  const lenderCommitmentGroupBeaconProxy = await hre.contracts.get('LenderCommitmentGroupBeacon')
+  const lenderCommitmentGroupV2BeaconProxy = await hre.contracts.get('LenderCommitmentGroupBeaconV2')
 
   const tellerV2 = await hre.contracts.get('TellerV2')
   const SmartCommitmentForwarder = await hre.contracts.get(
@@ -15,6 +15,9 @@ const deployFn: DeployFunction = async (hre) => {
   )
   const tellerV2Address = await tellerV2.getAddress()
 
+
+//   const uniswapPricingLibraryV2 = await hre.contracts.get('UniswapPricingLibraryV2')
+ 
   const smartCommitmentForwarderAddress =
     await SmartCommitmentForwarder.getAddress()
 
@@ -37,30 +40,32 @@ const deployFn: DeployFunction = async (hre) => {
     default:
       throw new Error('No swap factory address found for this network')
   }
-  const uniswapPricingLibraryV2 = await hre.deployments.get('uniswapPricingLibraryV2')
+
+
+  const uniswapPricingLibraryV2 = await hre.deployments.get('UniswapPricingLibraryV2')
 
  
 
 //this is why the owner of the beacon should be timelock controller ! 
 // so we can upgrade it like this . Using a proposal.  This actually goes AROUND the proxy admin, interestingly. 
   await hre.upgrades.proposeBatchTimelock({
-    title: 'LenderGroups: PoolOracle View',
+    title: 'Lender Pools V2: Upgrade Redeem Time',
     description: ` 
-# LenderGroups
+# Lender Pools V2
 
-* A patch to update the uniswap pricing library v2.
+* A patch to fix withdraw time delay requirements.
 `,
     _steps: [
       {
-        beacon: lenderCommitmentGroupBeaconProxy,
-        implFactory: await hre.ethers.getContractFactory('LenderCommitmentGroup_Smart', {
+        beacon: lenderCommitmentGroupV2BeaconProxy,
+        implFactory: await hre.ethers.getContractFactory('LenderCommitmentGroup_Pool_V2', {
           libraries: {
-            uniswapPricingLibraryV2: uniswapPricingLibraryV2.address,
+          UniswapPricingLibraryV2:   uniswapPricingLibraryV2.address,
           },
         }),
 
         opts: {
-          //  unsafeSkipStorageCheck: true, 
+          //unsafeSkipStorageCheck: true, 
           unsafeAllow: [
             'constructor',
             'state-variable-immutable',
@@ -85,14 +90,14 @@ const deployFn: DeployFunction = async (hre) => {
 }
 
 // tags and deployment
-deployFn.id = 'lender-commitment-group-beacon:upgrade-uniswap-price-lib-v2'
-deployFn.tags = ['lender-commitment-group-beacon']
+deployFn.id = 'lender-commitment-group-beacon-v2:upgrade-redeem-delay'
+deployFn.tags = ['lender-commitment-group-beacon-v2']
 deployFn.dependencies = [
   'teller-v2:deploy',
-  'teller-v2:uniswap-pricing-library-v2',
   'smart-commitment-forwarder:deploy',
-  'teller-v2:uniswap-pricing-library',
-  'lender-commitment-group-beacon:deploy'
+  'teller-v2:uniswap-pricing-library-v2',
+
+  'lender-commitment-group-beacon-v2:deploy'
 ]
 
 deployFn.skip = async (hre) => {

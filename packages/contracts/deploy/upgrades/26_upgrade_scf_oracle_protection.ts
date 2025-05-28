7646ed09bbe4630407dbfe98c851ebb8915eca5d
andy <admazzola@gmail.com>
2025-05-28 10:36:52 -0400

    update deploy scripts

---
+++ packages/contracts/deploy/upgrades/26_upgrade_scf_oracle_protection.ts
@@ -0,0 +1,65 @@
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+
+const deployFn: DeployFunction = async (hre) => {
+  hre.log('----------')
+  hre.log('')
+  hre.log('Smart Commitment Forwarder: Proposing upgrade...')
+
+  const tellerV2 = await hre.contracts.get('TellerV2')
+ 
+  const marketRegistry = await hre.contracts.get('MarketRegistry')
+
+  
+  const smartCommitmentForwarder = await hre.contracts.get(
+    'SmartCommitmentForwarder'
+  )
+ 
+
+  await hre.upgrades.proposeBatchTimelock({
+    title: 'Smart Commitment Forwarder: Upgrade Oracle Logic 3',
+    description: ` 
+# Smart Commitment Forwarder
+* Modifies Smart Commitment Forwarder to improve oracle security logic.
+`,
+    _steps: [
+      {
+        proxy: smartCommitmentForwarder,
+        implFactory: await hre.ethers.getContractFactory(
+          'SmartCommitmentForwarder'
+        ),
+
+        opts: {
+          unsafeAllow: ['constructor', 'state-variable-immutable'],
+          // unsafeAllowRenames: true,
+          // unsafeSkipStorageCheck: true, //caution !
+          constructorArgs: [
+            await tellerV2.getAddress(),
+            await marketRegistry.getAddress(),
+          ],
+            initializer : "initialize" 
+
+         
+           
+        },
+      },
+    ],
+  })
+
+  hre.log('done.')
+  hre.log('')
+  hre.log('----------')
+
+  return true
+}
+
+// tags and deployment
+deployFn.id = 'smart-commitment-forwarder:upgrade-oracle'
+deployFn.tags = ['proposal', 'upgrade', 'smart-commitment-forwarder-upgrade-oracle']
+deployFn.dependencies = ['smart-commitment-forwarder:deploy']
+deployFn.skip = async (hre) => {
+  
+  //only had to do this on polygon once 
+  return !hre.network.live || !['sepolia','polygon' ].includes(hre.network.name)
+}
+export default deployFn
+

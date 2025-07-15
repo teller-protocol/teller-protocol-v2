1690fb256a9c0d7b2fd2f28e7ad627d669669431
andy <admazzola@gmail.com>
2025-07-15 12:28:32 -0400

    add

--- packages/subgraph-substreamed-pool-v1/src/pb/mod.rs
+++ packages/subgraph-substreamed-pool-v1/src/pb/mod.rs
@@ -13,3 +13,16 @@ pub mod contract {
         // @@protoc_insertion_point(contract.v1)
     }
 }
+pub mod sf {
+    pub mod substreams {
+        pub mod sink {
+            pub mod database {
+                // @@protoc_insertion_point(attribute:sf.substreams.sink.database.v1)
+                pub mod v1 {
+                    include!("sf.substreams.sink.database.v1.rs");
+                    // @@protoc_insertion_point(sf.substreams.sink.database.v1)
+                }
+            }
+        }
+    }
+}

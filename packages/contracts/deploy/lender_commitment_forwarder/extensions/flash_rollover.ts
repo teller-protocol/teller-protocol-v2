1c027827e9df89a6e5db96f76b397b9cba751a8b
andy <admazzola@gmail.com>
2025-07-24 17:11:50 -0400

    bump v

--- packages/contracts/deploy/lender_commitment_forwarder/extensions/flash_rollover.ts
+++ packages/contracts/deploy/lender_commitment_forwarder/extensions/flash_rollover.ts
@@ -7,6 +7,7 @@ const aavePoolAddressProvider: { [networkName: string]: string } = {
   polygon: '0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb',
   arbitrum: '0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb',
   base: '0xe20fCBdBfFC4Dd138cE8b2E6FBb6CB49777ad64D',
+  // optimism: '0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb',
 }
 
 const networksWithAave: string[] = Object.keys(aavePoolAddressProvider)

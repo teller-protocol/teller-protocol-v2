56064d4a95fe60069945695d1f404f6a36dad9ac
andy <admazzola@gmail.com>
2025-07-29 09:57:03 -0400

    draft price adapter

---
+++ packages/contracts/contracts/price_adapters/PriceAdapterUniswapV3.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+ 
+
+// Interfaces
+import "./interfaces/IPriceAdapter.sol";
+
+ 
+contract PriceAdapterUniswapV3 is
+    IPriceAdapter,
+     
+{
+     
+
+   
+    mapping(bytes32 => bytes) public priceRoutes; 
+     
+    /* Modifiers */
+ 
+
+    /* Events */
+
+    event RouteRegistered(bytes32 hash, bytes route);
+   
+
+    /* External Functions */
+ 
+ 
+    function registerPriceRoute(
+       bytes[] route
+    ) external returns (bytes32 hash) {
+        
+
+    		// validate the route for length, other restrictions 
+
+
+    		// hash the route with keccak256 
+
+
+    		// store the route by its hash in the priceRoutes mapping 
+
+
+    }
+
+     
+    function getPrice(
+        bytes32 route , uint256 inAmount
+    ) external returns (uint256 marketId_) {
+        
+
+    	// lookup the route from the mapping 
+
+
+    	// use the route to query uniswapV3 for the price  using inAmount 
+
+
+
+    }
+ 
+}

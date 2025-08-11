b1717c49fc97f7b24bc7313ea5cdcfe96fe4da1f
andy <admazzola@gmail.com>
2025-08-11 14:03:40 -0400

    adding deploy scripts

---
+++ packages/contracts/deploy/pricing/uniswap_pricing_helper.ts
@@ -0,0 +1,14 @@
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+
+const deployFn: DeployFunction = async (hre) => {
+  const { deployer } = await hre.getNamedAccounts()
+  const UniswapPricingHelper = await hre.deployments.deploy('UniswapPricingHelper', {
+    from: deployer,
+  })
+}
+
+// tags and deployment
+deployFn.id = 'uniswap-pricing-helper:deploy'
+deployFn.tags = ['teller-v2', 'uniswap-pricing-helper:deploy']
+deployFn.dependencies = ['']
+export default deployFn

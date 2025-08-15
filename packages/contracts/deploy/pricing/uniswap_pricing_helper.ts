eace699fc48bc46510fccdf1aafa10130c1a91ac
andy <admazzola@gmail.com>
2025-08-15 11:22:32 -0400

    deloy

--- packages/contracts/deploy/pricing/uniswap_pricing_helper.ts
+++ packages/contracts/deploy/pricing/uniswap_pricing_helper.ts
@@ -2,13 +2,16 @@ import { DeployFunction } from 'hardhat-deploy/dist/types'
 
 const deployFn: DeployFunction = async (hre) => {
 
-    hre.log('Deploying UniswapPricingHelper')
-
+    
 
   const { deployer } = await hre.getNamedAccounts()
   const UniswapPricingHelper = await hre.deployments.deploy('UniswapPricingHelper', {
     from: deployer,
   })
+
+  hre.log('Deploying UniswapPricingHelper')
+
+
 }
 
 // tags and deployment

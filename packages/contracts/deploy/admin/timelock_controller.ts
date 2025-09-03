800e75483ce634ab0de0b42468de22337b0e3ee1
andy <admazzola@gmail.com>
2025-09-03 11:29:42 -0400

    timelock deploy script

---
+++ packages/contracts/deploy/admin/timelock_controller.ts
@@ -0,0 +1,56 @@
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+import { deploy } from 'helpers/deploy-helpers'
+
+const deployFn: DeployFunction = async (hre) => {
+
+
+	  const { protocolOwnerSafe } = await hre.getNamedAccounts()
+
+	  let protocolOwnerSafeAddress =   protocolOwnerSafe; 
+
+	  let minDelay = 180;
+
+	  let proposers = [ protocolOwnerSafeAddress ];
+	  let executors = [  protocolOwnerSafeAddress  ];
+	  let admin = protocolOwnerSafeAddress ; 
+
+
+
+  const timelock = await deploy({
+    contract: 'TimelockController',
+    args: [
+
+    	minDelay,
+    	proposers,
+    	executors,
+    	admin, 
+
+    ] ,
+    skipIfAlreadyDeployed: true,
+    hre,
+  })
+  
+
+   
+  return true
+}
+
+// tags and deployment
+deployFn.id = 'timelock-controller:deploy'
+deployFn.tags = ['timelock-controller', 'timelock-controller:deploy']
+deployFn.dependencies = []
+
+deployFn.skip = async (hre) => {
+
+  // return true;   //for now 
+  return !(
+    hre.network.live &&
+    [ 'hyperevm' ].includes(
+      hre.network.name
+    )
+  ) 
+
+
+}
+
+export default deployFn

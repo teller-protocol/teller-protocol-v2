6f939404b92e065f23e4acea7cba0717af415441
andy <admazzola@gmail.com>
2025-10-14 10:37:47 -0400

    improve test

--- packages/contracts/tests/LenderCommitmentForwarder/extensions/FlashRolloverLoan/SwapRolloverLoan_G2_Unit_Test.sol
+++ packages/contracts/tests/LenderCommitmentForwarder/extensions/FlashRolloverLoan/SwapRolloverLoan_G2_Unit_Test.sol
@@ -16,11 +16,21 @@ import { TellerV2SolMock } from "../../../../contracts/mock/TellerV2SolMock.sol"
 import { LenderCommitmentForwarderMock } from "../../../../contracts/mock/LenderCommitmentForwarderMock.sol";
 import { MarketRegistryMock } from "../../../../contracts/mock/MarketRegistryMock.sol";
 
+import { LenderCommitmentGroup_Pool_V2 } from "../../../../contracts/LenderCommitmentForwarder/extensions/LenderCommitmentGroup/LenderCommitmentGroup_Pool_V2.sol";
+
+import { ILenderCommitmentGroup_V2 } from "../../../../contracts/interfaces/ILenderCommitmentGroup_V2.sol";
+import { IUniswapPricingLibrary } from "../../../../contracts/interfaces/IUniswapPricingLibrary.sol";
+
+
+
+
 import { UniswapV3FactoryMock } from "../../../../contracts/mock/uniswap/UniswapV3FactoryMock.sol";
 import { UniswapV3PoolMock } from "../../../../contracts/mock/uniswap/UniswapV3PoolMock.sol";
 
 import {PoolAddress} from '../../../../contracts/libraries/uniswap/periphery/libraries/PoolAddress.sol';
 
+import { UniswapPricingHelper } from "../../../../contracts/price_oracles/UniswapPricingHelper.sol";
+import { SmartCommitmentForwarder } from "../../../../contracts/LenderCommitmentForwarder/SmartCommitmentForwarder.sol";
 
 
 contract SwapRolloverLoanOverride is SwapRolloverLoan_G2 {
@@ -91,6 +101,14 @@ contract SwapRolloverLoan_Unit_Test is Testable {
     LenderCommitmentForwarderMock lenderCommitmentForwarder;
     MarketRegistryMock marketRegistryMock;
 
+
+    SmartCommitmentForwarder smartCommitmentForwarder;
+
+    UniswapPricingHelper uniswapPricingHelper; 
+
+
+    LenderCommitmentGroup_Pool_V2 tellerPool; 
+
     function setUp() public {
         borrower = new User();
         lender = new User();
@@ -104,6 +122,7 @@ contract SwapRolloverLoan_Unit_Test is Testable {
 
         tellerV2.setMarketRegistry(address(marketRegistryMock));
 
+
         lenderCommitmentForwarder = new LenderCommitmentForwarderMock();
 
        
@@ -115,6 +134,14 @@ contract SwapRolloverLoan_Unit_Test is Testable {
          
         uniswapFactoryMock = new UniswapV3FactoryMock();
 
+        uniswapPricingHelper = new UniswapPricingHelper();
+
+        smartCommitmentForwarder = new SmartCommitmentForwarder( 
+             address(tellerV2),
+             address(marketRegistryMock) 
+            ); 
+
+       //  tellerV2.setTrustedMarketForwarder(  1,  address(smartCommitmentForwarder) );
 
 
         wethMock.deposit{ value: 100e18 }();
@@ -123,6 +150,44 @@ contract SwapRolloverLoan_Unit_Test is Testable {
         wethMock.transfer(address(lenderCommitmentForwarder), 5e18);
 
         wethMock.transfer(address(uniswapPoolMock), 5e18);
+
+ 
+        tellerPool = new LenderCommitmentGroup_Pool_V2(
+            address(tellerV2),
+            address(smartCommitmentForwarder),
+            address(uniswapFactoryMock),
+            address(uniswapPricingHelper)
+        );
+
+
+
+        ILenderCommitmentGroup_V2.CommitmentGroupConfig memory commitmentGroupConfig = ILenderCommitmentGroup_V2.CommitmentGroupConfig({
+            principalTokenAddress: address(wethMock) ,
+            collateralTokenAddress: address(collateralToken),
+            marketId: 1,
+            maxLoanDuration: 604800,
+            interestRateLowerBound: 100,
+            interestRateUpperBound: 1000,
+            liquidityThresholdPercent: 8000,
+            collateralRatio: 40000
+        });
+
+         IUniswapPricingLibrary.PoolRouteConfig[] memory routes = new IUniswapPricingLibrary.PoolRouteConfig[](1);
+
+         routes[0] = IUniswapPricingLibrary.PoolRouteConfig({
+            pool: 0xbe352daF66af94ccF2012a154a67DAEF95FAcB91,
+            zeroForOne: true,
+            twapInterval: 5,
+            token0Decimals: 18,
+            token1Decimals: 18
+        });
+
+
+           vm.prank( address(lender) );
+        tellerPool.initialize(
+            commitmentGroupConfig,
+            routes
+        ); 
 
       
 
@@ -227,6 +292,102 @@ contract SwapRolloverLoan_Unit_Test is Testable {
 
      
     }
+
+    function test_rolloverLoanWithFlashSwap_alternate_collateral() public {
+        address lendingToken = address(wethMock);
+
+        WethMock otherCollateralToken = new WethMock(); 
+
+        uint256 marketId = 0;
+        uint256 principalAmount = 5000;
+        uint256 flashAmount = 5000; 
+        uint32 duration = 1 days;
+        uint16 interestRate = 2000;
+
+        bytes32[] memory merkleProof; 
+
+        address rewardRecipient = address(0);
+        uint256 rewardAmount = 0; 
+
+
+
+        ILenderCommitmentForwarder.Commitment
+            memory commitment = ILenderCommitmentForwarder.Commitment({
+                maxPrincipal: principalAmount,
+                expiration: uint32(block.timestamp + 1 days),
+                maxDuration: duration,
+                minInterestRate: interestRate,
+                collateralTokenAddress: address( otherCollateralToken ),
+                collateralTokenId: 0,
+                maxPrincipalPerCollateralAmount: 0,
+                collateralTokenType: ILenderCommitmentForwarder
+                    .CommitmentCollateralType
+                    .NONE,
+                lender: address(lender),
+                marketId: marketId,
+                principalTokenAddress: lendingToken
+            });
+
+       //  lenderCommitmentForwarder.setCommitment(0, commitment);
+
+        SwapRolloverLoan_G2.AcceptCommitmentArgs
+            memory commitmentArgs = SwapRolloverLoan_G2.AcceptCommitmentArgs({
+                commitmentId: 0,
+                smartCommitmentAddress: address(  tellerPool   ),
+                principalAmount: principalAmount,
+                collateralAmount: 100,
+                collateralTokenId: 0,
+                collateralTokenAddress: address( collateralToken  ),
+                interestRate: interestRate,
+                loanDuration: duration,
+                merkleProof: merkleProof
+            });
+
+
+            SwapRolloverLoan_G2.FlashSwapArgs
+            memory flashSwapArgs = SwapRolloverLoan_G2.FlashSwapArgs({
+                token0: address(lendingToken),
+                token1: address(collateralToken),
+                fee: 100,
+                flashAmount: flashAmount,
+                borrowToken1: false 
+             //   poolAddress: address( uniswapPoolMock )
+            });
+
+        vm.prank(address(borrower));
+        uint256 loanId = tellerV2.submitBid(
+            lendingToken,
+            marketId,
+            principalAmount,
+            duration,
+            interestRate,
+            "",
+            address(borrower)
+        );
+
+     
+        uint256 borrowerAmount = 50; //have to pay the   fee..
+
+        vm.prank(address(borrower));
+        IERC20(lendingToken).approve(address(swapRolloverLoan), 1e18);
+
+        vm.prank(address(borrower));
+
+        swapRolloverLoan.rolloverLoanWithFlashSwap( 
+            address( smartCommitmentForwarder  ),
+            loanId, 
+            borrowerAmount,
+           // rewardAmount,
+        //    rewardRecipient, 
+
+            flashSwapArgs,
+            commitmentArgs
+        );
+
+     
+    }
+
+
 
 
       function test_calculateRolloverAmount() public {

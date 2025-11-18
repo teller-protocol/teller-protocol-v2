e4b5141c6df93ad26ce9095515ee84aa4c47d270
andy <admazzola@gmail.com>
2025-11-18 17:59:32 -0500

    v3 deploy scripts

---
+++ packages/contracts/contracts/LenderCommitmentForwarder/extensions/LenderCommitmentGroup/LenderCommitmentGroup_Factory_V3.sol
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Contracts
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol"; 
+
+import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// Interfaces
+import "../../../interfaces/ITellerV2.sol";
+import "../../../interfaces/IProtocolFee.sol";
+import "../../../interfaces/ITellerV2Storage.sol";
+import "../../../libraries/NumbersLib.sol";
+
+import {IUniswapPricingLibrary} from "../../../interfaces/IUniswapPricingLibrary.sol";
+
+import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+ 
+ import { IERC4626 } from  "../../../interfaces/IERC4626.sol";
+
+import { ILenderCommitmentGroup_V3 } from "../../../interfaces/ILenderCommitmentGroup_V3.sol";
+ 
+
+contract LenderCommitmentGroupFactory_V3 is OwnableUpgradeable {
+    using AddressUpgradeable for address;
+    using NumbersLib for uint256;
+
+    
+    //this is the beacon proxy
+    address public lenderGroupBeacon;
+  
+
+
+    mapping(address => uint256) public deployedLenderGroupContracts;
+
+    event DeployedLenderGroupContract(address indexed groupContract);
+
+ 
+
+      /**
+     * @notice Initializes the factory contract.
+     * @param _lenderGroupBeacon The address of the beacon proxy used for deploying group contracts.
+     */
+     function initialize(address _lenderGroupBeacon  )
+        external
+        initializer
+    {
+        lenderGroupBeacon = _lenderGroupBeacon; 
+      
+        __Ownable_init_unchained();
+    }
+
+
+    /**
+     * @notice Deploys a new lender commitment group pool contract.
+     * @dev The function initializes the deployed contract and optionally adds an initial principal amount.
+     * @param _initialPrincipalAmount The initial principal amount to be deposited into the group contract.
+     * @param _commitmentGroupConfig Configuration parameters for the lender commitment group.
+     * @param _poolOracleRoutes Array of pool route configurations for the Uniswap pricing library.
+     * @return newGroupContract_ Address of the newly deployed group contract.
+     */
+    function deployLenderCommitmentGroupPool(
+        uint256 _initialPrincipalAmount,
+        ILenderCommitmentGroup_V3.CommitmentGroupConfig calldata _commitmentGroupConfig,
+        IUniswapPricingLibrary.PoolRouteConfig[] calldata _poolOracleRoutes
+    ) external returns ( address ) {
+         
+  
+        BeaconProxy newGroupContract_ = new BeaconProxy(
+                lenderGroupBeacon,
+                abi.encodeWithSelector(
+                    ILenderCommitmentGroup_V3.initialize.selector,    //this initializes 
+                    _commitmentGroupConfig,
+                    _poolOracleRoutes
+
+                )
+            );
+
+        deployedLenderGroupContracts[address(newGroupContract_)] = block.number; //consider changing this ?
+        emit DeployedLenderGroupContract(address(newGroupContract_));
+
+ 
+
+        //it is not absolutely necessary to have this call here but it allows the user to potentially save a tx step so it is nice to have .
+         if (_initialPrincipalAmount > 0) {
+                _depositPrincipal(
+                address(newGroupContract_),
+                _initialPrincipalAmount,
+                _commitmentGroupConfig.principalTokenAddress 
+                
+            ); 
+        } 
+
+
+          //transfer ownership to msg.sender 
+        OwnableUpgradeable(address(newGroupContract_))
+            .transferOwnership(msg.sender);
+
+ 
+        return address(newGroupContract_) ;
+    }
+
+
+    /**
+     * @notice Adds principal tokens to a commitment group.
+     * @param _newGroupContract The address of the group contract to add principal tokens to.
+     * @param _initialPrincipalAmount The amount of principal tokens to add.
+     * @param _principalTokenAddress The address of the principal token contract.
+     */
+    function _depositPrincipal( 
+        address _newGroupContract,
+        uint256 _initialPrincipalAmount,
+        address _principalTokenAddress
+    ) internal returns (uint256) {
+
+
+        IERC20(_principalTokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            _initialPrincipalAmount
+        );
+        IERC20(_principalTokenAddress).approve(
+            _newGroupContract,
+            _initialPrincipalAmount
+        );
+
+        address sharesRecipient = msg.sender; 
+
+        uint256 sharesAmount_ = IERC4626( address(_newGroupContract) )
+            .deposit(
+                _initialPrincipalAmount,
+                sharesRecipient  
+            );
+
+        return sharesAmount_;
+    }
+
+}
---
+++ packages/contracts/deploy/lender_commitment_forwarder/extensions/lender_groups_v3/lender_commitment_group_v3_beacon.ts
@@ -0,0 +1,83 @@
+ 
+
+
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+
+import { UpgradeableBeacon } from 'types/typechain'
+
+import { get_ecosystem_contract_address } from "../../../../helpers/ecosystem-contracts-lookup"
+/*
+
+This deploys a one-off test contract of the lender commitment group contract !
+
+This is not needed for production 
+
+*/
+
+
+const deployFn: DeployFunction = async (hre) => {
+  const tellerV2 = await hre.contracts.get('TellerV2')
+  const SmartCommitmentForwarder = await hre.contracts.get(
+    'SmartCommitmentForwarder'
+  )
+
+
+  const tellerV2Address = await tellerV2.getAddress()
+
+
+ // const uniswapPricingLibraryV2 = await hre.contracts.get('UniswapPricingLibraryV2')
+
+   const priceAdapterAddress = await hre.contracts.get('PriceAdapterAerodrome')
+  
+
+  const smartCommitmentForwarderAddress =
+    await SmartCommitmentForwarder.getAddress()
+
+  let uniswapV3FactoryAddress  =  get_ecosystem_contract_address( hre.network.name, "uniswapV3Factory" ) ;
+   
+
+  const commitmentGroupBeacon = await hre.deployBeacon<UpgradeableBeacon>(
+    'LenderCommitmentGroup_Pool_V3',
+    {
+      customName: 'LenderCommitmentGroupBeaconV3',
+      unsafeAllow: ['constructor', 'state-variable-immutable','external-library-linking'],
+      constructorArgs: [
+        tellerV2Address,
+        smartCommitmentForwarderAddress,
+        priceAdapterAddress, 
+      ],
+       
+      
+    }
+  )
+
+ 
+  //is this necessary ? 
+  //isnt this just an implementation?
+   // this is necessary so only the protocol timelock can upgrade the beacon proxy 
+  
+  const { protocolTimelock , protocolOwnerSafe } = await hre.getNamedAccounts()
+  hre.log('Transferring ownership of CommitmentGroupBeacon to Gnosis Safe...')
+  await commitmentGroupBeacon.transferOwnership(protocolTimelock)
+  hre.log('done.')
+
+  return true
+}
+
+ 
+
+ 
+
+// tags and deployment
+deployFn.id = 'lender-commitment-group-beacon-v3:deploy'
+deployFn.tags = ['lender-commitment-group-beacon-v3']
+deployFn.dependencies = [
+  'teller-v2:deploy',
+  'smart-commitment-forwarder:deploy' 
+   
+]
+
+deployFn.skip = async (hre) => {
+  return !hre.network.live || !['sepolia', 'polygon' , 'mainnet','mainnet_live_fork','arbitrum','base','optimism','katana','hyperevm'].includes(hre.network.name)
+}
+export default deployFn
